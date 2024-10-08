b84203cb83284690a64e8815978a197fc4436069
WayanAndika <andikabuana45@gmail.com>
2024-10-08 11:39:29 +0800

    Final Version

---
+++ script/notification_script.js
@@ -0,0 +1,31 @@
+fetch('../data/kelas.json')
+  .then(response => response.json())
+  .then(data => {
+    const listMateri = document.querySelector('.list-materi'); // Pastikan class 'list-materi' ada di HTML Anda
+    data.kelas.forEach((kelas, index) => {
+      // Notifikasi
+      const containerMateri = document.createElement('div');
+      const accordionId = `accordion-flush-heading-${index + 1}`;
+      const bodyId = `accordion-flush-body-${index + 1}`;
+
+      containerMateri.innerHTML = `
+        <h2 id="${accordionId}">
+          <button type="button" class="flex items-center justify-between w-full py-5 font-medium rtl:text-right text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400 gap-3" data-accordion-target="#${bodyId}" aria-expanded="true" aria-controls="${bodyId}">
+            <span>${kelas.nama_mata_kuliah} ~ Pertemuan ${kelas.materi.pertemuan}</span>
+            <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
+            </svg>
+          </button>
+        </h2>
+        <div id="${bodyId}" class="hidden" aria-labelledby="${accordionId}">
+          <div class="py-5 border-b border-gray-200 dark:border-gray-700">
+            <p class="mb-2 text-gray-500 dark:text-gray-400">${kelas.materi.ringkasan_materi}</p>
+            <p class="text-sm text-gray-400">Waktu: ${kelas.waktu.mulai} - ${kelas.waktu.selesai} di ${kelas.tempat}</p>
+          </div>
+        </div>
+      `;
+
+      listMateri.appendChild(containerMateri);
+    });
+  })
+  .catch(error => console.error('Error fetching data:', error));
--- script/script.js
+++ script/script.js
@@ -1,27 +1,305 @@
 // Fetch data from the JSON file
-fetch('../data/data.json')
+fetch('../data/kelas.json')
   .then(response => response.json())
   .then(data => {
+    const containerClass = document.querySelector('.container-class');
+    let firstClass = {};
+    let secondClass = {};
+    let thirdClass = {};
+    const notif=document.querySelector('.notif');
+    notif.textContent=data.kelas.length
+    
     data.kelas.forEach(kelas => {
-        const container = document.querySelector('.other-class');
-        container.classList.add('card');
-        container.classList.add('bg-base-100');
-        container.classList.add('w-96');
-        container.classList.add('shadow-xl');
-        const otherClass=document.createElement('div')
-        otherClass.innerHTML = `
-                <div class="card-body">
-                    <p>Saat Ini</p>
-                    <h2 class="card-title">${kelas.nama_mata_kuliah}</h2>
-                    <p>${kelas.waktu.mulai}~${kelas.waktu.selesai}</p>
-                    <p>${kelas.tempat}</p>
-                    <p>${kelas.nama_dosen}</p>
-                </div>
-        `
-
-
-
-        container.appendChild(otherClass)
+        
+        if(kelas.id===1){
+          firstClass=kelas
+        }else if(kelas.id===2){
+          secondClass=kelas
+        }else{
+          thirdClass=kelas
+        }
     });
+
+    const todayClass=document.createElement('div');
+    todayClass.classList.add('grid','grid-cols-2','justify-between','gap-4','w-96', 'lg:w-full','mt-3','today-class');
+    const first=`
+      <div class="bg-primary rounded-lg p-4 lesson-1 grid justify-between first">
+            <span>Saat Ini</span>
+            <h2 class="text-2xl text-white mt-1 mb-3">${firstClass.nama_mata_kuliah}</h2>
+            <div class="time grid grid-row-1 grid-flow-col">
+                <img src="/images/time.svg" alt="" srcset="" >
+                <span class="col-span-6 text-white">${firstClass.waktu.mulai} ~ ${firstClass.waktu.selesai}</span>
+            </div>
+            <div class="room grid grid-row-1 grid-flow-col">
+                <img src="/images/room.svg" alt="" srcset="" >
+                <span class="col-span-7 text-white">${firstClass.tempat}</span>
+            </div>
+            <div class="people grid grid-row-1 grid-flow-col">
+                <img src="/images/people.svg" alt="" srcset="" >
+                <span class="col-span-6 text-white">${firstClass.nama_dosen}</span>
+            </div>
+      </div>
+    `;
+    const second=`
+      <div class="bg-gray-600 rounded-lg p-4 lesson-2 grid justify-between second">
+          <span>Selanjutnya</span>
+          <h2 class="text-2xl text-white mt-1 mb-3">${secondClass.nama_mata_kuliah}</h2>
+          <div class="time grid grid-row-1 grid-flow-col">
+              <img src="/images/time.svg" alt="" srcset="" >
+              <span class="col-span-6 text-white">${secondClass.waktu.mulai} ~ ${secondClass.waktu.selesai}</span>
+          </div>
+          <div class="room grid grid-row-1 grid-flow-col">
+              <img src="/images/room.svg" alt="" srcset="" >
+              <span class="col-span-7 text-white">${secondClass.tempat}</span>
+          </div>
+          <div class="people grid grid-row-1 grid-flow-col">
+              <img src="/images/people.svg" alt="" srcset="" >
+              <span class="col-span-6 text-white">${secondClass.nama_dosen}</span>
+          </div>
+      </div>
+    `
+    todayClass.innerHTML=`
+      ${first}
+      ${second}
+    `;
+    containerClass.appendChild(todayClass);
+
+
+    // Event Click
+    const previousClass=document.querySelector('.previous-class');
+    const nextClass=document.querySelector('.next-class');
+    // Previous
+    previousClass.addEventListener('click',()=>{
+      const todayClassContainer=document.querySelector('.today-class');
+      const lesson1=todayClassContainer.querySelector('.lesson-1');
+      const lesson2=todayClassContainer.querySelector('.lesson-2');
+      if(lesson1.classList.contains('bg-primary')){
+        todayClassContainer.innerHTML=`
+          <div class="bg-gray-600 rounded-lg p-4 lesson-1 grid justify-between third">
+              <span>Selanjutnya</span>
+              <h2 class="text-2xl text-white mt-1 mb-3">${thirdClass.nama_mata_kuliah}</h2>
+              <div class="time grid grid-row-1 grid-flow-col">
+                  <img src="/images/time.svg" alt="" srcset="" >
+                  <span class="col-span-6 text-white">${thirdClass.waktu.mulai} ~ ${thirdClass.waktu.selesai}</span>
+              </div>
+              <div class="room grid grid-row-1 grid-flow-col">
+                  <img src="/images/room.svg" alt="" srcset="" >
+                  <span class="col-span-7 text-white">${thirdClass.tempat}</span>
+              </div>
+              <div class="people grid grid-row-1 grid-flow-col">
+                  <img src="/images/people.svg" alt="" srcset="" >
+                  <span class="col-span-6 text-white">${thirdClass.nama_dosen}</span>
+              </div>
+          </div>
+          <div class="bg-primary rounded-lg p-4 lesson-2 grid justify-between first">
+            <span>Saat Ini</span>
+            <h2 class="text-2xl text-white mt-1 mb-3">${firstClass.nama_mata_kuliah}</h2>
+            <div class="time grid grid-row-1 grid-flow-col">
+                <img src="/images/time.svg" alt="" srcset="" >
+                <span class="col-span-6 text-white">${firstClass.waktu.mulai} ~ ${firstClass.waktu.selesai}</span>
+            </div>
+            <div class="room grid grid-row-1 grid-flow-col">
+                <img src="/images/room.svg" alt="" srcset="" >
+                <span class="col-span-7 text-white">${firstClass.tempat}</span>
+            </div>
+            <div class="people grid grid-row-1 grid-flow-col">
+                <img src="/images/people.svg" alt="" srcset="" >
+                <span class="col-span-6 text-white">${firstClass.nama_dosen}</span>
+            </div>
+          </div>
+        `
+      }else if(lesson2.classList.contains('bg-primary')){
+        todayClassContainer.innerHTML=`
+          <div class="bg-gray-600 rounded-lg p-4 lesson-1 grid justify-between second">
+          <span>Selanjutnya</span>
+          <h2 class="text-2xl text-white mt-1 mb-3">${secondClass.nama_mata_kuliah}</h2>
+          <div class="time grid grid-row-1 grid-flow-col">
+              <img src="/images/time.svg" alt="" srcset="" >
+              <span class="col-span-6 text-white">${secondClass.waktu.mulai} ~ ${secondClass.waktu.selesai}</span>
+          </div>
+          <div class="room grid grid-row-1 grid-flow-col">
+              <img src="/images/room.svg" alt="" srcset="" >
+              <span class="col-span-7 text-white">${secondClass.tempat}</span>
+          </div>
+          <div class="people grid grid-row-1 grid-flow-col">
+              <img src="/images/people.svg" alt="" srcset="" >
+              <span class="col-span-6 text-white">${secondClass.nama_dosen}</span>
+          </div>
+          </div>
+          <div class="bg-gray-600 rounded-lg p-4 lesson-2 grid justify-between third">
+          <span>Selanjutnya</span>
+          <h2 class="text-2xl text-white mt-1 mb-3">${thirdClass.nama_mata_kuliah}</h2>
+          <div class="time grid grid-row-1 grid-flow-col">
+              <img src="/images/time.svg" alt="" srcset="" >
+              <span class="col-span-6 text-white">${thirdClass.waktu.mulai} ~ ${thirdClass.waktu.selesai}</span>
+          </div>
+          <div class="room grid grid-row-1 grid-flow-col">
+              <img src="/images/room.svg" alt="" srcset="" >
+              <span class="col-span-7 text-white">${thirdClass.tempat}</span>
+          </div>
+          <div class="people grid grid-row-1 grid-flow-col">
+              <img src="/images/people.svg" alt="" srcset="" >
+              <span class="col-span-6 text-white">${thirdClass.nama_dosen}</span>
+          </div>
+      </div>
+          
+        `
+      }else{
+        todayClassContainer.innerHTML=`
+          <div class="bg-primary rounded-lg p-4 lesson-1 grid justify-between first">
+            <span>Saat Ini</span>
+            <h2 class="text-2xl text-white mt-1 mb-3">${firstClass.nama_mata_kuliah}</h2>
+            <div class="time grid grid-row-1 grid-flow-col">
+                <img src="/images/time.svg" alt="" srcset="" >
+                <span class="col-span-6 text-white">${firstClass.waktu.mulai} ~ ${firstClass.waktu.selesai}</span>
+            </div>
+            <div class="room grid grid-row-1 grid-flow-col">
+                <img src="/images/room.svg" alt="" srcset="" >
+                <span class="col-span-7 text-white">${firstClass.tempat}</span>
+            </div>
+            <div class="people grid grid-row-1 grid-flow-col">
+                <img src="/images/people.svg" alt="" srcset="" >
+                <span class="col-span-6 text-white">${firstClass.nama_dosen}</span>
+            </div>
+      </div>
+          <div class="bg-gray-600 rounded-lg p-4 lesson-2 grid justify-between second">
+          <span>Selanjutnya</span>
+          <h2 class="text-2xl text-white mt-1 mb-3">${secondClass.nama_mata_kuliah}</h2>
+          <div class="time grid grid-row-1 grid-flow-col">
+              <img src="/images/time.svg" alt="" srcset="" >
+              <span class="col-span-6 text-white">${secondClass.waktu.mulai} ~ ${secondClass.waktu.selesai}</span>
+          </div>
+          <div class="room grid grid-row-1 grid-flow-col">
+              <img src="/images/room.svg" alt="" srcset="" >
+              <span class="col-span-7 text-white">${secondClass.tempat}</span>
+          </div>
+          <div class="people grid grid-row-1 grid-flow-col">
+              <img src="/images/people.svg" alt="" srcset="" >
+              <span class="col-span-6 text-white">${secondClass.nama_dosen}</span>
+          </div>
+      </div>
+        `
+      }
+      
+    });
+    // Next 
+    nextClass.addEventListener('click',()=>{
+      const todayClassContainer=document.querySelector('.today-class');
+      const lesson1=todayClassContainer.querySelector('.lesson-1');
+      const lesson2=todayClassContainer.querySelector('.lesson-2');
+      if(lesson1.classList.contains('bg-primary')){
+        todayClassContainer.innerHTML=`
+          <div class="bg-gray-600 rounded-lg p-4 lesson-1 grid justify-between second">
+            <span>Selanjutnya</span>
+            <h2 class="text-2xl text-white mt-1 mb-3">${secondClass.nama_mata_kuliah}</h2>
+            <div class="time grid grid-row-1 grid-flow-col">
+                <img src="/images/time.svg" alt="" srcset="" >
+                <span class="col-span-6 text-white">${secondClass.waktu.mulai} ~ ${secondClass.waktu.selesai}</span>
+            </div>
+            <div class="room grid grid-row-1 grid-flow-col">
+                <img src="/images/room.svg" alt="" srcset="" >
+                <span class="col-span-7 text-white">${secondClass.tempat}</span>
+            </div>
+            <div class="people grid grid-row-1 grid-flow-col">
+                <img src="/images/people.svg" alt="" srcset="" >
+                <span class="col-span-6 text-white">${secondClass.nama_dosen}</span>
+            </div>
+          </div>
+          <div class="bg-gray-600 rounded-lg p-4 lesson-2 grid justify-between third">
+              <span>Selanjutnya</span>
+              <h2 class="text-2xl text-white mt-1 mb-3">${thirdClass.nama_mata_kuliah}</h2>
+              <div class="time grid grid-row-1 grid-flow-col">
+                  <img src="/images/time.svg" alt="" srcset="" >
+                  <span class="col-span-6 text-white">${thirdClass.waktu.mulai} ~ ${thirdClass.waktu.selesai}</span>
+              </div>
+              <div class="room grid grid-row-1 grid-flow-col">
+                  <img src="/images/room.svg" alt="" srcset="" >
+                  <span class="col-span-7 text-white">${thirdClass.tempat}</span>
+              </div>
+              <div class="people grid grid-row-1 grid-flow-col">
+                  <img src="/images/people.svg" alt="" srcset="" >
+                  <span class="col-span-6 text-white">${thirdClass.nama_dosen}</span>
+              </div>
+          </div>
+        `
+      }else if(lesson2.classList.contains('bg-primary')){
+        todayClassContainer.innerHTML=`
+          <div class="bg-primary rounded-lg p-4 lesson-1 grid justify-between first">
+            <span>Saat Ini</span>
+            <h2 class="text-2xl text-white mt-1 mb-3">${firstClass.nama_mata_kuliah}</h2>
+            <div class="time grid grid-row-1 grid-flow-col">
+                <img src="/images/time.svg" alt="" srcset="" >
+                <span class="col-span-6 text-white">${firstClass.waktu.mulai} ~ ${firstClass.waktu.selesai}</span>
+            </div>
+            <div class="room grid grid-row-1 grid-flow-col">
+                <img src="/images/room.svg" alt="" srcset="" >
+                <span class="col-span-7 text-white">${firstClass.tempat}</span>
+            </div>
+            <div class="people grid grid-row-1 grid-flow-col">
+                <img src="/images/people.svg" alt="" srcset="" >
+                <span class="col-span-6 text-white">${firstClass.nama_dosen}</span>
+            </div>
+          </div>
+          <div class="bg-gray-600 rounded-lg p-4 lesson-2 grid justify-between second">
+            <span>Selanjutnya</span>
+            <h2 class="text-2xl text-white mt-1 mb-3">${secondClass.nama_mata_kuliah}</h2>
+            <div class="time grid grid-row-1 grid-flow-col">
+                <img src="/images/time.svg" alt="" srcset="" >
+                <span class="col-span-6 text-white">${secondClass.waktu.mulai} ~ ${secondClass.waktu.selesai}</span>
+            </div>
+            <div class="room grid grid-row-1 grid-flow-col">
+                <img src="/images/room.svg" alt="" srcset="" >
+                <span class="col-span-7 text-white">${secondClass.tempat}</span>
+            </div>
+            <div class="people grid grid-row-1 grid-flow-col">
+                <img src="/images/people.svg" alt="" srcset="" >
+                <span class="col-span-6 text-white">${secondClass.nama_dosen}</span>
+            </div>
+          </div>
+          
+          
+        `
+      }else{
+        todayClassContainer.innerHTML=`
+          <div class="bg-gray-600 rounded-lg p-4 lesson-1 grid justify-between third">
+          <span>Selanjutnya</span>
+          <h2 class="text-2xl text-white mt-1 mb-3">${thirdClass.nama_mata_kuliah}</h2>
+          <div class="time grid grid-row-1 grid-flow-col">
+              <img src="/images/time.svg" alt="" srcset="" >
+              <span class="col-span-6 text-white">${thirdClass.waktu.mulai} ~ ${thirdClass.waktu.selesai}</span>
+          </div>
+          <div class="room grid grid-row-1 grid-flow-col">
+              <img src="/images/room.svg" alt="" srcset="" >
+              <span class="col-span-7 text-white">${thirdClass.tempat}</span>
+          </div>
+          <div class="people grid grid-row-1 grid-flow-col">
+              <img src="/images/people.svg" alt="" srcset="" >
+              <span class="col-span-6 text-white">${thirdClass.nama_dosen}</span>
+          </div>
+          </div>
+          <div class="bg-primary rounded-lg p-4 lesson-2 grid justify-between first">
+            <span>Saat Ini</span>
+            <h2 class="text-2xl text-white mt-1 mb-3">${firstClass.nama_mata_kuliah}</h2>
+            <div class="time grid grid-row-1 grid-flow-col">
+                <img src="/images/time.svg" alt="" srcset="" >
+                <span class="col-span-6 text-white">${firstClass.waktu.mulai} ~ ${firstClass.waktu.selesai}</span>
+            </div>
+            <div class="room grid grid-row-1 grid-flow-col">
+                <img src="/images/room.svg" alt="" srcset="" >
+                <span class="col-span-7 text-white">${firstClass.tempat}</span>
+            </div>
+            <div class="people grid grid-row-1 grid-flow-col">
+                <img src="/images/people.svg" alt="" srcset="" >
+                <span class="col-span-6 text-white">${firstClass.nama_dosen}</span>
+            </div>
+        `
+      }
+    });
+    
+
   })
   .catch(error => console.error('Error fetching data:', error));
+
+
+
+//
